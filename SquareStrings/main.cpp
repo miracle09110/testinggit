d9cc23cdd731b4024cdd102ff7dc2cac7b852843
miracle09110 <earon.pabon@gmail.com>
2017-04-20 10:20:22 +0800

    Add files via upload

---
+++ SquareStrings/main.cpp
@@ -0,0 +1,43 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+string str ="";
+getline (cin, str);
+int l = str.length(); // value for length of string
+int substrcounter = 0; // position in substring array
+int k = (((l*(l+1))/2)-1); // size of substring array
+string substrings [k] = {};
+bool x = false;
+
+for (int strcounter=0; strcounter<=l; strcounter++)
+    for (int strlength=1; strlength <=l && strlength<= l-strcounter; strlength++){
+    substrings[substrcounter]= str.substr(strcounter,strlength);
+    substrcounter++;
+}
+
+
+for (int h=1; h<=k; h++) { // h is to denote position of element being compared to other elements
+    for (int j=h+1; j<=k; j++){ // j is to denote position of other element being compare too
+
+        if(substrings[h]==substrings[j]){
+            x= true;
+            break;
+        }
+
+
+}
+}
+
+if (x==true){
+cout << str << " -Not Square Free" << endl;
+}
+
+else {
+    cout << str << " - Square Free" << endl;
+    }
+}
